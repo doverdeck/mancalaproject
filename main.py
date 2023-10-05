84539e30906e42553a1a1e7abf88abc870069ac5
doverdeck <d.l.overdeck@gmail.com>
2023-10-05 11:58:18 -0400

    board

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import *
+# Create the main window
+root = tk.Tk()
+root.title("Tic tac toe")
+counter=0
+b=0
+c=0
+# Create a function to handle button clicks
+def button_click(b,c):
+    print(f"{c}")
+    print(f"{b}")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Create a 2x7 grid of buttons
+buttons = [[1, 2, 3, 4, 5, 6, 7],[1, 2, 3, 4, 5, 6, 7]]
+grid = [[0, 4, 4, 4, 4, 4, 4],[0, 4, 4, 4, 4, 4, 4]]
+for i in range(2):
+    b=0
+    for j in range(7):
+        if (i!=0):
+            button = tk.Button(root, width=10, height=3,text=f"Blank", command=lambda b=b,c=c: button_click(b,c))
+        else:
+            button = tk.Button(root, width=10, height=6, text=f"Blank", command=lambda b=b, c=c: button_click(b, c))
+        b=b+1
+        print(f"{b}")
+        button.grid(row=i, column=j)
+        buttons[i][j] = button
+    c = c + 1
+# Start the tkinter main loop
+root.mainloop()
