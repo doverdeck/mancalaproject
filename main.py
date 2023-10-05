38cb18240d48a401130bc0ab7f0df9f066089fda
doverdeck <d.l.overdeck@gmail.com>
2023-10-05 19:28:56 -0400

    board

--- main.py
+++ main.py
@@ -10,7 +10,48 @@ c=0
 def button_click(b,c):
     print(f"{c}")
     print(f"{b}")
-
+    row=c
+    col=b
+    for i in range(grid[c][b]):
+        grid[c][b]=grid[c][b]-1
+        if(row==0):
+            if(col+1>6):
+                col=6
+                row=1
+                grid[1][6]=grid[1][6]+1
+            else:
+                grid[row][col+1]=grid[row][col+1]+1
+                col=col+1
+        elif(row==1):
+            if (col-1<0):
+                col = 0
+                row = 0
+                grid[0][0]=grid[0][0]+1
+            else:
+                grid[row][col-1] = grid[row][col - 1]+1
+                col = col-1
+    print(f"grid={grid[c][b]}")
+    check_winner()
+    update_text()
+def update_text():
+    for i in range(2):
+        for j in range(7):
+            buttons[i][j].config(text=f"{grid[i][j]}")
+def check_winner():
+    sum=0
+    for i in range(6):
+        sum=sum+grid[0][i+1]
+    sum2=0
+    for j in range(6):
+        sum2=sum2+grid[1][j]
+    if(sum==0):
+        grid[1][6]=sum2+grid[1][6]
+        for j in range(6):
+            grid[1][j]=0
+    elif(sum2==0):
+        grid[0][0] = sum + grid[0][0]
+        for i in range(6):
+            grid[0][i + 1]=0
 # Create a 2x7 grid of buttons
 buttons = [[1, 2, 3, 4, 5, 6, 7],[1, 2, 3, 4, 5, 6, 7]]
 grid = [[0, 4, 4, 4, 4, 4, 4],[4, 4, 4, 4, 4, 4, 0]]
@@ -18,9 +59,9 @@ for i in range(2):
     b=0
     for j in range(7):
         if ((i==0 and j==0)or(i==1 and j==6)):
-            button = tk.Button(root, width=10, height=12,text=f"Blank", command=lambda b=b,c=c: button_click(b,c))
+            button = tk.Button(root, width=10, height=12,text=f"{grid[c][b]}")
         else:
-            button = tk.Button(root, width=10, height=3, text=f"Blank", command=lambda b=b, c=c: button_click(b, c))
+            button = tk.Button(root, width=10, height=4, text=f"{grid[c][b]}", command=lambda b=b, c=c: button_click(b, c))
         b=b+1
         print(f"{b}")
         button.grid(row=i, column=j)
